ed6251da3c2f51992e13846db41b4a4b11e92b2e
FrancoMusolino <francomusolino55@gmail.com>
2022-06-27 22:23:57 -0300

    Cleaning CRA

---
+++ src/styles/theme.js
@@ -0,0 +1,10 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const colors = {
+  brand: {
+    primary: "#F63356",
+    background: "#1A1A1A",
+  },
+};
+
+export const theme = extendTheme({ colors });
